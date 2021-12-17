36ae1c714735e148656d7a867ae51d88ea190d6f
Danielscript1 <danielsousagit@gmail.com>
2021-12-17 12:47:16 -0300

    simplificandoPaginacaoOrdenacaoAjust2

--- src/main/java/com/testeweb/course/controller/TopicosController.java
+++ src/main/java/com/testeweb/course/controller/TopicosController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
@@ -44,7 +45,8 @@ public class TopicosController {
 	
 	//adicionando paginacao
 	@GetMapping
-	public Page<TopicoDto> lista(@RequestParam(required=false) String nomeCurso,Pageable paginacao) {
+	public Page<TopicoDto> lista(@RequestParam(required=false) String nomeCurso,
+		@PageableDefault(sort="id",direction = Direction.DESC,page=0,size=10)	Pageable paginacao) {
 		//adicionando interface de paginação com springData
 	
 		if (nomeCurso == null) {
