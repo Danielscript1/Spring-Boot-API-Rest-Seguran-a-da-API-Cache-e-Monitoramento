8c2948e07aea608876b0743ba5ff027f905674f1
Danielscript1 <danielsousagit@gmail.com>
2021-12-20 11:07:42 -0300

    LiberandoAcessoEndpoints

--- src/main/java/com/testeweb/course/config/security/SecurityConfigurations.java
+++ src/main/java/com/testeweb/course/config/security/SecurityConfigurations.java
@@ -1,11 +1,34 @@
 package com.testeweb.course.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
-	
+	//configuraçoes de autentificação
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(auth);
+	}
+	//configuracoes de autorização 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests()
+		.antMatchers(HttpMethod.GET,"/topicos").permitAll()
+		.antMatchers(HttpMethod.GET,"/topicos/*").permitAll();
+		
+	}
+	//configuracoes recursos estaticos sao requisicoes para arquivos ->js ,css,imagens
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// TODO Auto-generated method stub
+		super.configure(web);
+	}
 }
